bb561334af10aff931b2fb0744fbbf9e509b1714
advnpzn <adenosinetp10@protonmail.com>
2024-07-07 21:47:58 -0700

    sample bot

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from telegram.ext import Application
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from config import Config
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 cfg = Config()
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    logging.info("Sending hello message.")
+    await context.bo
+    logging.info("Done :)")
+
+if __name__ == '__main__':
+    
+    app = ApplicationBuilder().token(cfg.BOT_TOKEN).build()
+    app.add_handler(CommandHandler('hello', hello))
+
+    app.run_polling()
